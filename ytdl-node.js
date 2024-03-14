96a5f056e7e05fb825f08ea41da85d543fc5040c
Almafazi <almafazi@icloud.com>
2024-03-15 05:18:50 +0700

    sikat

---
+++ ytdl-node.js
@@ -0,0 +1,155 @@
+const fastify = require('fastify');
+const { spawn } = require('child_process');
+const Queue = require('bull');
+const extractYoutubeId = require('youtube-video-id').default;
+const { createBullBoard } = require('@bull-board/api');
+const { BullAdapter } = require('@bull-board/api/bullAdapter');
+const { FastifyAdapter } = require('@bull-board/fastify');
+const fs = require('fs');
+const path = require('path');
+const fastifyCors = require('@fastify/cors');
+
+const serverAdapter = new FastifyAdapter();
+serverAdapter.setBasePath('/admin/queues');
+
+const queue = new Queue('yt-dlp-conversion', {
+    redis: {
+        host: 'localhost',
+        port: 6379,
+    },
+});
+
+createBullBoard({
+  queues: [new BullAdapter(queue)],
+  serverAdapter: serverAdapter,
+});
+
+const app = fastify();
+
+app.register(fastifyCors, {
+    origin: /\.snaptik\.vip$/,
+});
+
+serverAdapter.setBasePath('/ui');
+app.register(serverAdapter.registerPlugin(), { prefix: '/ui' });
+
+app.post('/convert', async (request, reply) => {
+    const { youtubeUrl } = request.body;
+    const youtubeId = extractYoutubeId(youtubeUrl);
+    if (!youtubeUrl) {
+        reply.status(400).send({ message: 'youtubeUrl is required' });
+        return;
+    }
+    if (!validateYouTubeUrl(youtubeUrl)) {
+
+        reply.status(400).send({ message: 'Invalid youtubeUrl' });
+        return;
+    }
+
+    const outputPath = `./converted/${youtubeId}/%(title)s.%(ext)s`;
+
+    const job = await queue.add({ youtubeUrl, outputPath, directoryPath: `./converted/${youtubeId}` }, {jobId: youtubeId, removeOnComplete: true, removeOnFail: true});
+    
+    reply.send({ jobId: job.id });
+});
+
+app.get('/check/:jobId', async (request, reply) => {
+    const { jobId } = request.params;
+    const job = await queue.getJob(jobId);
+
+    if (!job) {
+        reply.status(404).send({ message: 'Job not found' });
+        return;
+    }
+
+    const progress = await job.progress();
+    const status = await job.getState();
+
+    reply.send({ progress, status });
+});
+
+app.get('/download/:jobId', async (request, reply) => {
+    const { jobId } = request.params;
+    const job = await queue.getJob(jobId);
+
+    if (!job) {
+        reply.status(404).send({ message: 'Job not found' });
+        return;
+    }
+
+    const status = await job.getState();
+
+    if (status !== 'completed') {
+        reply.status(400).send({ message: 'Job is not completed yet' });
+        return;
+    }
+
+    const outputPath = job.data.outputPath;
+    const directoryPath = job.data.directoryPath;
+    const downloadUrl = generateDownloadUrl(outputPath, directoryPath);
+
+    reply.send({ downloadUrl });
+});
+
+function generateDownloadUrl(outputPath, directoryPath) {
+    const files = fs.readdirSync(directoryPath);
+    const mp3Files = files.filter(file => path.extname(file) === '.mp3');
+    if (mp3Files.length === 0) {
+        throw new Error('No MP3 files found in the directory');
+    }
+    const mp3File = mp3Files[0];
+    const downloadUrl = path.join(directoryPath, mp3File);
+    return downloadUrl;
+}
+
+async function convertToMp3(youtubeUrl, outputPath) {
+    return new Promise((resolve, reject) => {
+        const process = spawn('./yt-dlp.sh', [
+            '-f', 'bestaudio/best',
+            '--extract-audio',
+            '--audio-format', 'mp3',
+            '--audio-quality', '192',
+            '--embed-thumbnail', // Add this line to enable adding album art
+            '-o', outputPath,
+            youtubeUrl
+        ]);
+
+        process.on('exit', (code) => {
+            if (code === 0) {
+                resolve();
+            } else {
+                reject(new Error(`Conversion failed with exit code ${code}`));
+            }
+        });
+    });
+}
+
+queue.process(3, async (job) => {
+    const { youtubeUrl, outputPath } = job.data;
+
+    try {
+        await convertToMp3(youtubeUrl, outputPath);
+    } catch (error) {
+        console.error(`Failed to convert ${youtubeUrl} to MP3: ${error.message}`);
+        throw error;
+    }
+});
+
+function validateYouTubeUrl(urlToParse){
+    if (urlToParse) {
+        var regExp = /^(?:https?:\/\/)?(?:m\.|www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/;
+        if (urlToParse.match(regExp)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+app.listen({ port: 3007 }, (err) => {
+    if (err) {
+        console.error('Failed to start server:', err);
+        process.exit(1);
+    }
+
+    console.log('Server is listening on port 3000');
+});
