663e6039b11d2a8c836c86340ffe4e9f5c1b95c2
Almafazi <almafazi@icloud.com>
2024-03-17 01:31:43 +0700

    gash

--- ytdl-node-schedule.js
+++ ytdl-node-schedule.js
@@ -2,20 +2,20 @@ const cron = require('node-cron');
 const findRemoveSync = require('find-remove');
 const Queue = require('bull');
 
-cron.schedule('*/10 * * * *', () => {
+cron.schedule('*/5 * * * *', () => {
     const directory = `${__dirname}/converted`; 
 
     const queue = new Queue('yt-dlp-conversion', {
         redis: {
             host: 'localhost',
             port: 6379,
-            // password: '!Rahman214'
+            password: '!Rahman214'
         },
     });
     checkDiskSpace('/').then((diskSpace) => {
         const freeSpaceInPercent = Math.round((diskSpace.free / diskSpace.size) * 100);
-        if(freeSpaceInPercent < 35) {
-            const result = findRemoveSync(directory, { age: { hours: 3 }, dir: '*' });
+        if(freeSpaceInPercent < 25) {
+            const result = findRemoveSync(directory, { age: { minutes: 30 }, dir: '*' });
             if(result) {
                 Object.keys(result).forEach((file) => {
                     const fileName = file.split('/').pop();
