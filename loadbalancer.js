ebf754b94d25de60d168e32eeff3f96795adfc34
Almafazi <almafazi@icloud.com>
2024-03-18 05:04:01 +0700

    ghas

--- loadbalancer.js
+++ loadbalancer.js
@@ -62,7 +62,8 @@ app.use(async (req, res, next) => {
     proxy.on('proxyRes', function (proxyRes, req, res) {
         res.setHeader('X-Server-URL', server.url);
     });
-    proxy.web(req, res, { target: server.url, rejectUnauthorized: false });
+    proxy.web(req, res, { target: server.url, rejectUnauthorized: false,   changeOrigin: true,
+    });
 });
 
 app.listen(3033);
