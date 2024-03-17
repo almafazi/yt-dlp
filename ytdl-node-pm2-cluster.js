07f65879e9ddf51d15586bdcad78fbb9afe490e3
Almafazi <almafazi@icloud.com>
2024-03-17 13:13:05 +0700

    max 15

--- ytdl-node-pm2-cluster.js
+++ ytdl-node-pm2-cluster.js
@@ -177,7 +177,7 @@ require('dotenv').config()
         return new Promise((resolve, reject) => {
             const proxyUrl = 'http://hwbknjxk-rotate:wcpjh6lq5loy@p.webshare.io:80';
             const process = spawn('./yt-dlp.sh', [
-                '--break-match-filters', 'duration <= 800',
+                '--break-match-filters', 'duration <= 950',
                 '-f', 'bestaudio/best',
                 '--extract-audio',
                 '--audio-format', 'mp3',
