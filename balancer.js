84f0a57c26f0dc169666c3d4197a74392968021a
Almafazi <almafazi@icloud.com>
2024-03-19 00:46:57 +0700

    gas

--- balancer.js
+++ balancer.js
@@ -124,6 +124,17 @@ app.use(async (req, res, next) => {
 
 });
 
+proxy.on('proxyReq', (proxyReq, req) => {
+    if (req.body) {
+        const bodyData = JSON.stringify(req.body);
+        // incase if content-type is application/x-www-form-urlencoded -> we need to change to application/json
+        proxyReq.setHeader('Content-Type','application/json');
+        proxyReq.setHeader('Content-Length', Buffer.byteLength(bodyData));
+        // stream the content
+        proxyReq.write(bodyData);
+    }
+});
+
 function validateYouTubeUrl(urlToParse) {
     if (urlToParse) {
         var regExp = /^(?:https?:\/\/)?(?:m\.|www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/;
