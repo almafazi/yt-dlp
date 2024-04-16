bd3e4f09150af0214f65d1c64158bf50425893a3
Almafazi <almafazi@icloud.com>
2024-04-16 09:07:02 +0700

    tes

--- ttdl-stream.mjs
+++ ttdl-stream.mjs
@@ -56,6 +56,7 @@ app.get('/directdownload', async (req, res) => {
 
         // Set headers based on the response from the got stream
         stream.on('response', (responseStream) => {
+            console.log(responseStream.headers['content-length'])
             res.setHeader('Content-Length', responseStream.headers['content-length']);
             res.setHeader('Content-Transfer-Encoding', 'Binary');
             res.setHeader('Content-Type', 'application/octet-stream');
