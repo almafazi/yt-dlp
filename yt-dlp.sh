faf245536f39cdcbeea158e41a65a6e46240c2a7
Almafazi <almafazi@icloud.com>
2024-06-10 21:05:00 +0700

    ghas

--- yt-dlp.sh
+++ yt-dlp.sh
@@ -1,11 +1,2 @@
 #!/usr/bin/env sh
-
-# Determine which Python version to use
-if command -v python3.10 &>/dev/null; then
-    PYTHON_VERSION="python3.10"
-else
-    PYTHON_VERSION="python3"
-fi
-
-# Execute yt-dlp with the chosen Python version, redirecting Python output to /dev/null
-exec ${PYTHON:-$PYTHON_VERSION} -Werror -Xdev "$(dirname "$(realpath "$0")")/yt_dlp/__main__.py" "$@" >/dev/null
+exec ${PYTHON:-python3.10} -Werror -Xdev "$(dirname "$(realpath "$0")")/yt_dlp/__main__.py" "$@"
