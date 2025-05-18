5a27a0be1cf17f1328257574672ed47de46488dd
Almafazi <almafazi@icloud.com>
2025-05-18 11:53:28 +0700

    fix

--- yt_dlp_plugins/extractor/getpot_bgutil.py
+++ yt_dlp_plugins/extractor/getpot_bgutil.py
@@ -1,13 +1,17 @@
 from __future__ import annotations
 
-__version__ = '0.8.4'
+__version__ = '0.8.5'
 
 import typing
 
 if typing.TYPE_CHECKING:
     from yt_dlp import YoutubeDL
-# NOTE: this is internal only and may be moved in the future
-from yt_dlp.networking._helper import select_proxy
+
+try:
+    from yt_dlp.networking._helper import select_proxy
+except ImportError:
+    from yt_dlp.utils.networking import select_proxy
+
 from yt_dlp.networking.common import Features
 from yt_dlp.networking.exceptions import UnsupportedRequest
 from yt_dlp.utils import classproperty, remove_end
